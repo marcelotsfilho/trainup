ae64a9b3447db6a4ecd90b57eb6450b2589e7aeb
Rafaela Lorenzeto <rafaelaobraga@hotmail.com>
2025-10-31 16:34:47 -0300

    Alterações para a tela de Pagamentos

--- src/app/(tabs)/payment.tsx
+++ src/app/(tabs)/payment.tsx
@@ -1,21 +1,23 @@
-import PaymentsPaid from '@/src/components/PaymentsPaid';
 import Constants from "expo-constants";
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 const statusBarHeight = Constants.statusBarHeight;
+const HEADER_HEIGHT = 160;
 
 export default function Payment(){
     return(
+        //A view principal da tela
         <View className="flex-1 items-center bg-[#E5EAF2]">
-            <View className="w-full h-40 bg-[#3A7DFF]">
-                <View
-                    className="flex-row items-center ml-8 "
-                    style={{marginTop: statusBarHeight+10}}
-                >
-                </View>
+            <View>
+                <Text>Eu sou o conteúdo da tela</Text>
             </View>
-            <PaymentsPaid title='Histórico de Pagamento' vencimento={''} valor={''}/>
+            //A view do Header
+            <View className="w-full bg-[#3A7DFF] absolute top-0 items-center" style={{ height: HEADER_HEIGHT }}>
+                <View className="flex-row items-center" style={{marginTop: statusBarHeight + 10}}>
+                    <Text className="text-white">Aqui ficam os botões</Text>
+                </View>
+            </View>
         </View>
     );
 }
