8e58248846641089c5d31c9b53089cfd1b020cfa
Rafaela Lorenzeto <rafaelaobraga@hotmail.com>
2025-10-31 21:35:07 -0300

    Implementação das telas de Pagos e Abertos

---
+++ src/components/PaymentOpenList.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import dadosJSON from '../data/pagamentosAbertos.json';
+import PaymentsPay from "./PaymentsPay";
+
+export default function PaymentOpenList() {
+    const [listaDePagamentosAbertos, setListaDePagamentosAbertos] = useState([] as typeof dadosJSON);
+
+    useEffect(() => {
+        setListaDePagamentosAbertos(dadosJSON);
+    }, []);
+
+    return (
+        <ScrollView>
+            {listaDePagamentosAbertos.length === 0 ? (
+                <View className="w-full flex items-center justify-center mb-5">
+                    <Icon name="shield-check-outline" size={40} color="#3A7DFF" />
+                    <Text className="text-lg font-semibold text-black mt-2">Nenhum registro</Text>
+                    <Text className="text-sm text-gray-600 mt-1 mb-6">Não há nenhum pagamento aberto</Text>
+                </View>
+            )
+                : listaDePagamentosAbertos.map((pagamento, index) => (
+                    <View key={index} className="mb-4">
+                        <PaymentsPay
+                            title={`Mensalidade Ref: ${pagamento.mes}`}
+                            vencimento={pagamento.dataVenc}
+                            valor={pagamento.valor}
+                        />
+                    </View>
+                ))}
+        </ScrollView>
+    );
+}
---
+++ src/components/PaymentsPay.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+
+
+export default function PaymentsPay({
+    title,
+    vencimento,
+    valor,
+}: {
+    title: string;
+    vencimento: string;
+    valor: string;
+}
+) {
+    return (
+        <View className="w-[328px] h-[90px] bg-white rounded-[10px] flex-row items-center px-4 mx-auto">
+            <View className="w-5 h-5 bg-[#1ED760] rounded-full" />
+            <View className="ml-4">
+                <Text className="text-[#000000] font-[SF Pro] text-base font-semibold leading-[22px] tracking-normal">{title}</Text>
+                <Text className="text-[#000000] font-[SF Pro] text-base font-semibold leading-[22px] tracking-normal">Vencimento: {vencimento}</Text>
+                <Text className="text-[#000000] font-[SF Pro] text-base font-semibold leading-[22px] tracking-normal">Valor: R$ {valor}</Text>
+            </View>
+        </View>
+    );
+}
